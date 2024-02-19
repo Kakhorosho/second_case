21de91f1b7fd12308bbd0a3124a7bf079a5ba1d7
Wolgerrom5 <sofyaviktorovna26@gmail.com>
2024-02-19 23:37:51 +0700

    1 one

--- Sofa.py
+++ Sofa.py
@@ -1,1 +1,12 @@
-print('hgjoj')
+import sympy as sy
+import numpy as n
+import matplotlib as m
+import pandas as p
+
+n = sy.Symbol(n)
+def progression(a1, d, n, ):
+    S = ((2 * a1 + d(n - 1)) * n) / 2
+    return n
+progression(1, 1, n, 105)
+print('')
+ 

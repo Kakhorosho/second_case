1e28c57541bcb67ccbe2cd25b5c2b56074286215
margosh1234r5563 <m.rusakova@g.nsu.ru>
2024-02-25 03:53:07 +0700

    add file

--- Ann's.py
+++ Ann's.py
@@ -1,7 +1,8 @@
-import sympy as sy
-import ru_local as ru
 import matplotlib.pyplot as plt
 import numpy as np
+import sympy as sy
+
+import ru_local as ru
 
 
 def qd(q1, q2, p1, p2):
@@ -119,6 +120,7 @@ def days(qe, a_1, a_n):
     print(f'{ru.AMOUNT_DAYS} {n} {ru.NAME_DAYS}')
     return n
 
+
 def qd_new(q1, q2, p1, p2, new_volume):
     b = (q2 - q1) / (p2 - p1)
     a = q1 - b * p1 + new_volume
@@ -126,10 +128,12 @@ def qd_new(q1, q2, p1, p2, new_volume):
     print(f'{ru.NEW_DEMAND_FUNCTION} qd = {int(a)}{int(b)}pd')
     return a + b * pd
 
+
 def p_new(q1, q2, p1, p2, q, new_volume):
     b = (q2 - q1) / (p2 - p1)
     a = q1 - b * p1 + new_volume
     return (q - a) / b
+
 
 def p_new_print(q1, q2, p1, p2, q, new_volume):
     b = (q2 - q1) / (p2 - p1)
@@ -137,25 +141,31 @@ def p_new_print(q1, q2, p1, p2, q, new_volume):
     print(f'{ru.NEW_REVERSE_DEMAND_FUNCTION} p = (q - {int(a)})/{int(b)}')
     return (q - a) / b
 
+
 def tr_new(qx_new):
     px_new = p_new(150, 100, 50, 60, qx_new, 300)
     tr_new = px_new * qx_new
     return tr_new
 
+
 def mr_new(q):
     mr_new = sy.diff(tr_new(q), q)
     return mr_new
+
 
 def mr_new_print(q):
     mr_new = sy.diff(tr_new(q), q)
     print(f'{ru.NEW_MARGINAL_REVENUE} {mr_new}')
     return mr_new
+
+
 def qe_new():
     q = sy.Symbol("q")
     mr_new_1 = mr_new(q)
     mc_1 = mc_previous(q)
     qe_new = sy.solve(mc_1 - mr_new_1, q)
     return qe_new[0]
+
 
 def qe_new_print():
     q = sy.Symbol("q")
@@ -172,11 +182,13 @@ def pe_new():
     pe_new = p_new(150, 100, 50, 60, q_equal_new, 300)
     return pe_new
 
+
 def pe_new_print():
     q_equal_new = qe_new()
     pe_new = p_new(150, 100, 50, 60, q_equal_new, 300)
     print(f'{ru.NEW_MARKET_EQUILIBRIUM_PRICE} {int(pe_new)} {ru.CURRENCY}')
     return pe_new
+
 
 def new_profit():
     q_equal_new = qe_new()
@@ -185,6 +197,7 @@ def new_profit():
     new_profit = tr_1_new - tc_1
     new_profit = int(new_profit)
     return new_profit
+
 
 def new_profit_print():
     q_equal_new = qe_new()
@@ -205,34 +218,29 @@ pe_print()
 tc_print(q, 80)
 profit_print()
 print(ru.POINT_B)
-days(105,1,14)
+days(105, 1, 14)
 print(ru.POINT_C)
-qd_new(150,100,50,60,300 )
-p_new_print(150,100,50,60, q, 300)
+qd_new(150, 100, 50, 60, 300)
+p_new_print(150, 100, 50, 60, q, 300)
 mr_new_print(q)
 qe_new_print()
 pe_new_print()
 new_profit_print()
 print(f'{ru.CHANGE_PROFIT} {new_profit() - profit()} {ru.CURRENCY}')
 
-
 plt.figure()
 plt.xlabel('Q')
 plt.ylabel('P')
 
-
 q_value = np.linspace(0, 200, 400)
 
-
-profit_value = [tr(q) - tc(q,80) for q in q_value]
+profit_value = [tr(q) - tc(q, 80) for q in q_value]
 plt.plot(q_value, profit_value)
 plt.text(-8, 3000, 'Profit')
-
 
 mc_value = [mc_previous(q) for q in q_value]
 plt.plot(q_value, mc_value)
 plt.text(200, 500, 'MC')
-
 
 q1 = 150
 q2 = 100
@@ -240,31 +248,27 @@ p1 = 50
 p2 = 60
 b = (q2 - q1) / (p2 - p1)
 a = q1 - b * p1
-p1=[]
-q1=[]
+p1 = []
+q1 = []
 
-for i in range(0,200):
-  p1.append(i)
-  q1.append(a + b * i)
-plt.plot(p1,q1)
+for i in range(0, 200):
+    p1.append(i)
+    q1.append(a + b * i)
+plt.plot(p1, q1)
 plt.text(200, -1000, 'Qd')
-
 
 tr_value = [tr(q) for q in q_value]
 plt.plot(q_value, tr_value)
 plt.text(200, 8500, 'TR')
 
-
 mr_value = np.gradient(tr(q_value), q_value)
 plt.plot(q_value, mr_value)
 plt.text(200, -300, 'MR')
 
-
 q_new_value = np.linspace(165, 210, 400)
-tc_value = [tc(q,80) for q in q_new_value]
+tc_value = [tc(q, 80) for q in q_new_value]
 plt.plot(q_new_value, tc_value)
 plt.text(205, 6000, 'TC')
-
 
 # Task C
 q1 = 150
@@ -273,22 +277,21 @@ p1 = 50
 p2 = 60
 b = (q2 - q1) / (p2 - p1)
 a = q1 - b * p1 + 300
-p1=[]
-q1=[]
+p1 = []
+q1 = []
 
-for i in range(0,200):
-  p1.append(i)
-  q1.append(a + b * i)
-plt.plot(p1,q1)
+for i in range(0, 200):
+    p1.append(i)
+    q1.append(a + b * i)
+plt.plot(p1, q1)
 plt.text(25, 1000, "Qd'")
 
-
-q_new_value_1 = np.linspace(0,200,400)
+q_new_value_1 = np.linspace(0, 200, 400)
 tr_value_new = [tr_new(q) for q in q_new_value_1]
 plt.plot(q_new_value_1, tr_value_new)
 plt.text(105, 13500, "TR'")
 
-profit_value_new = [tr_new(q) - tc(q,80) for q in q_value]
+profit_value_new = [tr_new(q) - tc(q, 80) for q in q_value]
 plt.plot(q_value, profit_value_new)
 plt.text(90, 20000, "Profit'")
 
